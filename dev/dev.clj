8b614d4805400947506d43c2c600a5051156f297
Adrian Medina <adrian@wikipunk.net>
2022-12-26 15:14:19 -0500

    data quality vocabulary

--- dev/dev.clj
+++ dev/dev.clj
@@ -64,6 +64,7 @@
    [net.wikipunk.rdf.geo :as geo]
    [net.wikipunk.rdf.swrl :as swrl]
    [net.wikipunk.rdf.dcat :as dcat]
+   [net.wikipunk.rdf.dqv :as dqv]
    ;; LV2 RDF models
    [net.wikipunk.rdf.lv2.atom :as atom]
    [net.wikipunk.rdf.lv2.bufsz :as bufsz]
